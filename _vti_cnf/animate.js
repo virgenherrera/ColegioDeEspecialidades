3f8752908666c4d2652f0c21c876767ba0cc67db
Hugo Virgen <virgenherrera@gmail.com>
2016-09-19 21:59:31 -0500

    rel01

---
+++ _vti_cnf/animate.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|PENTIUM4HT\\Hugo
+vti_modifiedby:SR|PENTIUM4HT\\Hugo
+vti_timelastmodified:TR|11 Mar 2007 03:06:45 -0000
+vti_timecreated:TR|11 Mar 2007 03:06:45 -0000
+vti_cacheddtm:TX|11 Mar 2007 03:06:45 -0000
+vti_filesize:IR|16355
+vti_extenderversion:SR|6.0.2.5516
+vti_backlinkinfo:VX|index.htm
+vti_syncwith_colegiodeespecialidades.com\:80:TW|11 Mar 2007 03:06:45 -0000
+vti_syncofs_colegiodeespecialidades.com\:21/www:TW|11 Mar 2007 17:42:00 -0000
+vti_syncwith_www.colegiodeespecialidades.com\:80:TW|11 Mar 2007 03:06:45 -0000
---
+++ espanol/Datos/js/_vti_cnf/statusbarmsg.js
@@ -0,0 +1,14 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|20 Dec 2006 11:48:14 -0000
+vti_extenderversion:SR|5.0.2.2623
+vti_lineageid:SR|{1C43D540-ED7F-4323-AF5C-4DA3CAE4EE45}
+vti_backlinkinfo:VX|index.htm
+vti_author:SR|COMPÃš\\Hugo
+vti_modifiedby:SR|COMPÃš\\Hugo
+vti_nexttolasttimemodified:TW|20 Dec 2006 11:46:56 -0000
+vti_timecreated:TR|20 Dec 2006 11:43:11 -0000
+vti_cacheddtm:TX|20 Dec 2006 11:48:14 -0000
+vti_filesize:IR|1599
+vti_syncwith_www.espanol.colegiodeespecialidades.com\:80:TR|20 Dec 2006 11:48:14 -0000
+vti_syncofs_colegiodeespecialidades.com\:21/www:TW|11 Mar 2007 17:45:00 -0000
+vti_syncwith_colegiodeespecialidades.com\:21/www:TX|20 Dec 2006 11:48:14 -0000
